0a1a097f98b66babec0e4a2151042a5322c3b363
yuni <hys990730@gmail.com>
2024-03-08 00:52:57 +0900

    backjoon 1931.py

--- greedy/1931.py
+++ greedy/1931.py
@@ -1,14 +1,10 @@
 n = int(input())
-meet = [tuple(map(int, input().split())) for _ in range(n)]
-meet.sort(key=lambda x: (x[1], x[0]))
-count = 1 if meet[0][1] != 0 else 0
-compare = meet[0][1]
-
-for i in range(1, n):
-    if compare <= meet[i][0]:
-        count += 1
-        compare = meet[i][1]
-    else:
-        continue
+p = list(map(int, input().split()))
     
-print(count)
+p.sort()
+result = 0
+
+for i in range(n):
+    result += p[i]*(n-i)
+    
+print(result)
