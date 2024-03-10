92cb973dafccca8a5184d2772c235e35f8008b52
YUNI Heo <ys8973@kangwon.ac.kr>
2024-03-10 23:14:01 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,15 +1,7 @@
-def solution(numer1, denom1, numer2, denom2):
-    new_numer = numer1 * denom2 + numer2 * denom1
-    new_denom = denom1 * denom2
-    
-    greatest_common_divisor = gcd(new_numer, new_denom)
-    
-    result_numer = new_numer // greatest_common_divisor
-    result_denom = new_denom // greatest_common_divisor
-    
-    return [result_numer, result_denom]
+import math
 
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+def solution(numer1, denom1, numer2, denom2):
+    n = numer1 * denom2 + numer2 * denom1
+    d = denom1 * denom2
+
+    return [n // math.gcd(n, d), d // math.gcd(n, d)]
