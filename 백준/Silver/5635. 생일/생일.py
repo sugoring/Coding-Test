2109414cd168dbe13474f241db95a5d9885c8fab
YUNI Heo <ys8973@kangwon.ac.kr>
2024-08-10 20:11:47 +0900

    [Silver V] Title: 생일, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -6,10 +6,15 @@ members = []
 
 for _ in range(number):
     name, dd, mm, yyyy = sys.stdin.readline().split()
-    members.append([name, int(dd), int(mm), int(yyyy)])
+    if (len(dd) == 1):
+        dd = "0" + dd
+    if (len(mm) == 1):
+        mm = "0" + mm
+    yyyymmdd = yyyy + mm + dd
+    
+    members.append([name, int(yyyymmdd)])
 
-# 한 번의 sort에서 여러 키를 사용하여 정렬합니다.
-members.sort(key=lambda x: (x[3], x[2], x[1]))
+members.sort(key=lambda x: x[1])
 
 print(members[-1][0])
 print(members[0][0])
